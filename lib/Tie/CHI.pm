0816ba300a0c845d49f49f82ba1dd594ea5572e7
Jonathan Swartz <swartz@pobox.com>
2011-10-06 15:58:24 -0700

    various

--- lib/Tie/CHI.pm
+++ lib/Tie/CHI.pm
@@ -1,7 +1,65 @@
 package Tie::CHI;
-use 5.008;
+use CHI;
 use strict;
 use warnings;
+
+sub TIEHASH {
+    my ( $class, $cache ) = @_;
+
+    if ( ref($cache) eq 'HASH' ) {
+        $cache = CHI->new(%$cache);
+    }
+    elsif ( !( blessed($cache) && $cache->isa('CHI::Driver') ) ) {
+        die "must pass a hash of options or a CHI object";
+    }
+    my $self = bless { _cache => $cache }, $class;
+    return $self;
+}
+
+sub _cache {
+    return $_[0]->{_cache};
+}
+
+sub STORE {
+    my ( $self, $key, $value ) = @_;
+    $self->_cache->set( $key, $value );
+}
+
+sub FETCH {
+    my ( $self, $key ) = @_;
+    return $self->_cache->get($key);
+}
+
+sub FIRSTKEY {
+    my ($self) = @_;
+    $self->{_keys_iterator} = $self->_cache->get_keys_iterator();
+    return $self->{_keys_iterator}->();
+}
+
+sub NEXTKEY {
+    my ($self) = @_;
+    return $self->{_keys_iterator}->();
+}
+
+sub EXISTS {
+    my ( $self, $key ) = @_;
+    return $self->_cache->is_valid($key);
+}
+
+sub DELETE {
+    my ( $self, $key ) = @_;
+    $self->_cache->remove($key);
+}
+
+sub CLEAR {
+    my ($self) = @_;
+    $self->_cache->clear();
+}
+
+sub SCALAR {
+    my ($self) = @_;
+    defined( $self->FIRSTKEY );
+}
 
 1;
 
@@ -17,13 +75,57 @@ Tie::CHI - Tied hash to persistent CHI cache
 
     use Tie::CHI;
 
+    my %cache;
+
+    # Choose a driver and options
+    #
+    tie %cache, 'Tie::CHI', { driver => 'File', root_dir => '/path/to/root' };
+    tie %cache, 'Tie::CHI', {
+        namespace => 'homepage',
+        driver  => 'Memcached::libmemcached',
+        servers => [ "10.0.0.15:11211", "10.0.0.15:11212" ],
+        default_expires_in => '10 min'
+    );
+
+    # Cache operations
+    #
+    my $customer = $cache{$name};
+    if ( !defined $customer ) {
+        $customer = get_customer_from_db($name);
+        $cache{$name} = $customer;
+    }
+    ...
+    delete($cache{$name});
+
 =head1 DESCRIPTION
 
-Tie::CHI provides
+Tie::CHI implements a tied hash connected to a L<CHI|CHI> cache. It can be used
+with any of CHI's backends (L<File|CHI::Driver::File>,
+L<Memcached|CHI::Driver::Memcached>, L<DBI|CHI::Driver::DBI>, etc.) Simply pass
+a hashref of CHI parameters to tie:
 
-Questions and feedback are welcome, and should be directed to the mailing list:
+A read/write/delete on the tied hash will result in a C<get>/C<set>/C<remove>
+on the underlying cache. C<keys> will be supported if the underlying CHI driver
+supports C<get_keys> (e.g. File and DBI, not Memcached).
 
-    http://groups.google.com/...
+There is no way to specify expiration for an individual C<set>, but you can
+pass C<expires_in>, C<expires_at> and/or C<expires_variance> to the tie to
+specify default expiration. e.g.
+
+    tie %cache, 'Tie::CHI', { 
+        namespace => 'products',
+        driver => 'DBI',
+        dbh => DBIx::Connector->new(...),
+        expires_in => '4 hours',
+        expires_variance => '0.2'
+    };
+
+=head1 SUPPORT AND DOCUMENTATION
+
+Questions and feedback are welcome, and should be directed to the perl-cache
+mailing list:
+
+    http://groups.google.com/group/perl-cache-discuss
 
 Bugs and feature requests will be tracked at RT:
 
@@ -32,10 +134,10 @@ Bugs and feature requests will be tracked at RT:
 
 The latest source code can be browsed and fetched at:
 
-    http://github.com/jonswar/perl-tie-chi
+    http://github.com/jonswar/perl-tie-chi/tree/master
     git clone git://github.com/jonswar/perl-tie-chi.git
 
 =head1 SEE ALSO
 
-L<Some::Module>
+L<CHI|CHI>
 
--- lib/Tie/CHI/Util.pm
+++
@@ -1,9 +0,0 @@
-package Tie::CHI::Util;
-use strict;
-use warnings;
-use base qw(Exporter);
-
-our @EXPORT_OK = qw(
-);
-
-1;
